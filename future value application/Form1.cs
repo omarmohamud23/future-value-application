c9f84d2d0eee1d5006f1ac9b21831c7115e295f4
uiefheuifhyeuihyfr <weyrhwehyruihefuihdsuifhwu>
2020-02-13 12:17:26 -0600

    Add project files.

---
+++ future value application/Form1.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace future_value_application
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox4_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnCalculate_Click(object sender, EventArgs e)
+        {
+            decimal monthlyInvestment =
+                Convert.ToDecimal(txtMonthlyIvestment.Text);
+            decimal yearlyInterestRate = Convert.ToDecimal(txtInterestRate.Text);
+            int years = Convert.ToInt32(txtYears.Text);
+
+            int months = years * 12;
+            decimal monthlyInterestRate = yearlyInterestRate / 12 / 100;
+
+            decimal futureValue = 0m;
+            for (int i = 0; i < months; i++)
+            {
+                futureValue = (futureValue + monthlyInvestment)
+                            * (1 + monthlyInterestRate);
+            }
+            txtFutureValue.Text = futureValue.ToString("C");
+            txtMonthlyIvestment.Focus();
+        }
+
+       
+
+        private decimal CalculateFuturevalue(decimal monthlyInvestment, decimal monthlyInterestRate, int months)
+        {
+            decimal futureValue = 0m;
+            for (int i = 0; i < months; i++)
+            {
+                futureValue = (futureValue + monthlyInvestment)
+                            * (1 + monthlyInvestment);
+            }
+            return futureValue;
+        }
+    
+
+      
+
+       
+        private void btnExit_Click_1(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
